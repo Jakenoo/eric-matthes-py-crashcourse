5752ddde91208096375d3230607aa3b174cb298b
Jacob Perez <jvcp1145@gmail.com>
2024-01-28 18:41:58 -0800

    Updated exercise 9-3 as specified by 9-5. Just adds some methods for counting user login attempts

--- ch9_exercises/9-3.py
+++ ch9_exercises/9-3.py
@@ -1,4 +1,6 @@
 # 1/28/2024; a short exercise from the section on classes (p. 162)
+# updated the same date according to exercise 9-5 instructions
+
 class User:
     """A short user class maybe simulating a profile on a website"""
     def __init__(self, first_name, last_name, age, birthday):
@@ -6,14 +8,28 @@ class User:
         self.last_name = last_name.title()
         self.age = age
         self.birthday = birthday
+        self.login_attempts = 0
     def describe_user(self):
         print(f"Here is information on {self.first_name} " + 
               f"{self.last_name}:")
         print(f"Age: {self.age}\nBirthday: {self.birthday}")
     def greet_user(self):
         print(f"Hello, {self.first_name}!")
+    
+    # block for login-related data
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 user1 = User("mr", "michi", 2, "02/28/2023")
-user2 = User("mrs", "puff", 40, "08/07/1999")
 user1.greet_user()
 user1.describe_user()
+
+user2 = User("mrs", "puff", 40, "08/07/1999")
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+print(user2.login_attempts)
+user2.reset_login_attempts()
+print(user2.login_attempts)
