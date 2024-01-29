cbc4af570feb3762d478ce334094905df001cc55
Jacob Perez <jvcp1145@gmail.com>
2024-01-29 11:06:17 -0800

    added a separate Privileges class to handle the show_privileges() method

--- ch9_exercises/admin.py
+++ ch9_exercises/admin.py
@@ -1,14 +1,20 @@
 # 1/29/2024; module for Admin subclass of User superclass
 import user
-class Admin(user.User):
-    def __init__(self, first_name, last_name, age, birthday):
-        super().__init__(first_name, last_name, age, birthday)
-        self.privileges = ["can add post", "can delete post", "can ban user", 
-                           "can make announcements"]
+
+class Privileges():
+    """class for creating privileges attribute in Admint class"""
+    def __init__(self, privileges = ["can add post", "can delete post", "can ban user", 
+                           "can make announcements"]):
+        self.privileges = privileges
     def show_privileges(self):
         print("Here are the current privileges for admins:")
         for privilege in self.privileges:
             print(f"\t{privilege}")
 
+class Admin(user.User):
+    def __init__(self, first_name, last_name, age, birthday):
+        super().__init__(first_name, last_name, age, birthday)
+        self.privileges = Privileges()
+
 first_admin = Admin("Captain", "Qwark", "n/a", "11/04/2002")
-first_admin.show_privileges()
+first_admin.privileges.show_privileges()
