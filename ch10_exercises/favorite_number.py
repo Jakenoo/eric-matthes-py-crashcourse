dcfa5b774b5d5cac6feab131f41f9aca76d89874
Jacob Perez <jvcp1145@gmail.com>
2024-02-20 21:06:49 -0800

    added exercise 10-11

---
+++ ch10_exercises/favorite_number.py
@@ -0,0 +1,22 @@
+#2/20/2024; exercise 10-11
+
+from pathlib import Path
+import json
+
+def store_favorite_number(path):
+    favorite_num = input("What is your favorite number? ")
+    path.write_text(json.dumps(favorite_num))
+    print(f"Your favorite number ({favorite_num}) has been stored.")
+
+def get_favorite_number(path):
+    favorite_num = json.loads(path.read_text())
+    print(f"Your favorite number is {favorite_num}!")
+
+def read_favorite_number():
+    path = Path("favorite_number.json")
+    if path.exists():
+        get_favorite_number(path)
+    else:
+        store_favorite_number(path)
+
+read_favorite_number()
