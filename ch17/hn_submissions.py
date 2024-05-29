2ed3f991a353f4b10c42c7faf7dfee38a90e1072
Jacob Perez <jvcp1145@gmail.com>
2024-05-29 11:36:49 -0700

    added more examples from ch17 I forgot to commit a few days ago

---
+++ ch17/hn_submissions.py
@@ -0,0 +1,34 @@
+#26/5/2024; more API practice
+from operator import itemgetter
+import requests
+
+#Make API call, verify response
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+#format info from each submission
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:5]:
+    #Make new API call per submission
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    #build dictionary per article
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descendants'],
+    }
+submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                          reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
